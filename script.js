3d2a0cf3570f49d3ea2ddc4f669eb94eb353d583
mak20th <mak20th@gmail.com>
2019-06-10 09:04:42 -0500

    added render shopping list

--- script.js
+++ script.js
@@ -19,37 +19,66 @@ const STORE = [
     {name: 'oranges', checked: false},
     {name: 'milk', checked: true},
     {name: 'bread', checked: false}
-  ];
+];
+
+function generateItemElement(item, itemIndex, template) {
+    return `
+    <li data-item-id="${item.id}">
+      <span class="shopping-item js-shopping-item ${item.checked ? 'shopping-item__checked' : ''}">${item.name}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle js-item-toggle">
+            <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete js-item-delete">
+            <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>`;
+}
+
+function generateShoppingItemsString(shoppingList) {
+    console.log('Generating shopping list element');
+    //we create a new array of item strings (items) by mapping over shoppingList and calling a new function, 
+    //generateItemElement on each item. 
+    const items = shoppingList.map((item, index) => generateItemElement(item, index));
+    //will return a single string that joins together the individual item strings
+    return items.join('');
+}
+  
 function renderShoppingList() {
     // this function will be responsible for rendering the shopping list in the DOM
     console.log('`renderShoppingList` ran');
-  }
-  
-  
-  function handleNewItemSubmit() {
+    const shoppingListItemsString = generateShoppingItemsString(STORE);
+
+    // insert that HTML into the DOM
+    $('.js-shopping-list').html(shoppingListItemsString);
+}
+
+
+function handleNewItemSubmit() {
     // this function will be responsible for when users add a new shopping list item
     console.log('`handleNewItemSubmit` ran');
-  }
-  
-  
-  function handleItemCheckClicked() {
+}
+
+
+function handleItemCheckClicked() {
     // this function will be responsible for when users click the "check" button on
     // a shopping list item.
     console.log('`handleItemCheckClicked` ran');
-  }
-  
-  
-  function handleDeleteItemClicked() {
+}
+
+
+function handleDeleteItemClicked() {
     // this function will be responsible for when users want to delete a shopping list
     // item
     console.log('`handleDeleteItemClicked` ran')
-  }
-  
-  // this function will be our callback when the page loads. it's responsible for
-  // initially rendering the shopping list, and activating our individual functions
-  // that handle new item submission and user clicks on the "check" and "delete" buttons
-  // for individual shopping list items.
-  function handleShoppingList() {
+}
+
+// this function will be our callback when the page loads. it's responsible for
+// initially rendering the shopping list, and activating our individual functions
+// that handle new item submission and user clicks on the "check" and "delete" buttons
+// for individual shopping list items.
+function handleShoppingList() {
     renderShoppingList();
     handleNewItemSubmit();
     handleItemCheckClicked();
